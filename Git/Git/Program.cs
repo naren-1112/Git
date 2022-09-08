0239ec5d04d1b4557684b9c265a58da0ec1cffe1
NAREN <sk.narencric@gmail.com>
2022-09-08 11:13:09 +0530

    commit

---
+++ Git/Git/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("D:\\GIT\\Git\\welcome.txt");
+            Console.WriteLine(data);
+            Console.ReadLine();
+        }
+    }
+}
