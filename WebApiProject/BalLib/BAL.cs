507ee5ced403aafaf74c9a078144513fe1556af6
NAREN <sk.narencric@gmail.com>
2022-10-16 18:22:19 +0530

    commit

---
+++ WebApiProject/BalLib/BAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalLib
+{
+    public class BAL
+    {
+        public int student_id { get; set; }
+        public string student_name { get; set; }
+        public int subject_marks { get; set; }
+    }
+}
---
+++ WebApiProject/HelperLib/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BalLib;
+using DalLib;
+
+namespace HelperLib
+{
+    public class Helper
+    {
+        DAL dal = null;
+        public Helper()
+        {
+            dal = new DAL();
+        }
+
+
+        public bool Addmarks(BAL employee)
+        {
+            return dal.Insert(employee);
+
+        }
+
+        public bool Editmarks(BAL employee)
+        {
+            return dal.Update(employee);
+        }
+
+        public BAL searchmarks(int empid)
+        {
+            return dal.Find(empid);
+        }
+        public List<BAL> listmarks()
+        {
+            return dal.list();
+        }
+        public bool removemarks(int employee_id)
+        {
+            return dal.Delete(employee_id);
+        }
+    }
+}
